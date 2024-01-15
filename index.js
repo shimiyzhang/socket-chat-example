9c0e85459cc61ac4763a97aa6f5f243055e91d19
zhangshumeng <916406821@qq.com>
2024-01-15 17:17:32 +0800

    feat: 🎸 Step #3: Integrating Socket.IO

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ import express from "express";
 import { createServer } from "node:http";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
+import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
   res.sendFile(join(_dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
