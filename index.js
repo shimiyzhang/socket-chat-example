44a856502feb1e1410702875dade88e61b05301c
zhangshumeng <916406821@qq.com>
2024-01-15 17:06:29 +0800

    feat: 🎸 Step #2: Serving HTML

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import { createServer } from 'node:http';
+import express from "express";
+import { createServer } from "node:http";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 const app = express();
 const server = createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(_dirname, "index.html"));
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
