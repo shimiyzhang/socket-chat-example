d5440223e386a960dc484f5c2ccc644bbf109cf1
zhangshumeng <916406821@qq.com>
2024-01-16 11:47:59 +0800

    feat: 🎸 Step #7: Server delivery

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ import { createServer } from "node:http";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { Server } from "socket.io";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+// open the database file
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_offset TEXT UNIQUE,
+    content TEXT
+  );
+`);
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  connectionStateRecovery: {
-    // the backup duration of the sessions and the packets
-    maxDisconnectionDuration: 2 * 60 * 1000,
-    // whether to skip middlewares upon successful recovery
-    skipMiddlewares: true,
-  }
+  connectionStateRecovery: {},
 });
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
@@ -21,10 +33,33 @@ app.get("/", (req, res) => {
   res.sendFile(join(_dirname, "index.html"));
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      // store the message in the database
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (e) {
+      // TODO handle the failure
+      return;
+    }
+    // include the offset with the message
+    io.emit("chat message", msg, result.lastID);
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+      await db.each('SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      )
+    } catch (e) {
+      // something went wrong
+    }
+  }
 });
 
 server.listen(3000, () => {
