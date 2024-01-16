62348b81839379af3b06f7cc3629ada39f4714bf
zhangshumeng <916406821@qq.com>
2024-01-16 11:26:45 +0800

    feat: 🎸 Step #6: Connection state recovery

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ import { Server } from "socket.io";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {
+    // the backup duration of the sessions and the packets
+    maxDisconnectionDuration: 2 * 60 * 1000,
+    // whether to skip middlewares upon successful recovery
+    skipMiddlewares: true,
+  }
+});
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
