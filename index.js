d20bc2305e78d2d228680b9407d17f5252536fb5
zhangshumeng <916406821@qq.com>
2024-01-16 13:53:31 +0800

    feat: 🎸 Step #9: Scaling horizontally

--- index.js
+++ index.js
@@ -1,79 +1,89 @@
 import express from "express";
 import { createServer } from "node:http";
-import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 import sqlite3 from "sqlite3";
 import { open } from "sqlite";
+import { availableParallelism } from "node:os";
+import cluster from "node:cluster";
+import { createAdapter, setupPrimary } from "@socket.io/cluster-adapter";
 
-// open the database file
-const db = await open({
-  filename: "chat.db",
-  driver: sqlite3.Database,
-});
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i,
+    });
+  }
 
-// create our 'messages' table (you can ignore the 'client_offset' column for now)
-await db.exec(`
-  CREATE TABLE IF NOT EXISTS messages (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    client_offset TEXT UNIQUE,
-    content TEXT
-  );
-`);
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-  connectionStateRecovery: {},
-});
-
-const _dirname = dirname(fileURLToPath(import.meta.url));
-
-app.get("/", (req, res) => {
-  res.sendFile(join(_dirname, "index.html"));
-});
-
-io.on("connection", async (socket) => {
-  socket.on("chat message", async (msg, clientOffset, callback) => {
-    let result;
-    try {
-      // store the message in the database
-      result = await db.run(
-        "INSERT INTO messages (content) VALUES (?)",
-        msg,
-        clientOffset
-      );
-    } catch (e) {
-      if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
-        // the message was already inserted, so we notify the client
-        callback();
-      } else {
-        // nothing to do, just let the client retry
-      }
-      return;
-    }
-    // include the offset with the message
-    io.emit("chat message", msg, result.lastID);
-    // acknowledge the event
-    callback();
+  setupPrimary();
+} else {
+  const db = await open({
+    filename: "chat.db",
+    driver: sqlite3.Database,
   });
 
-  if (!socket.recovered) {
-    // if the connection state recovery was not successful
-    try {
-      await db.each(
-        "SELECT id, content FROM messages WHERE id > ?",
-        [socket.handshake.auth.serverOffset || 0],
-        (_err, row) => {
-          socket.emit("chat message", row.content, row.id);
-        }
-      );
-    } catch (e) {
-      // something went wrong
-    }
-  }
-});
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+    );
+  `);
 
-server.listen(3000, () => {
-  console.log("server running at http://localhost:3000");
-});
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {},
+    adapter: createAdapter(),
+  });
+
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  app.get("/", (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+  });
+
+  io.on("connection", async (socket) => {
+    socket.on("chat message", async (msg, clientOffset, callback) => {
+      let result;
+      try {
+        result = await db.run(
+          "INSERT INTO messages (content, client_offset) VALUES (?, ?)",
+          msg,
+          clientOffset
+        );
+      } catch (e) {
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */) {
+          callback();
+        } else {
+          // nothing to do, just let the client retry
+        }
+        return;
+      }
+      io.emit("chat message", msg, result.lastID);
+      callback();
+    });
+
+    if (!socket.recovered) {
+      try {
+        await db.each(
+          "SELECT id, content FROM messages WHERE id > ?",
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit("chat message", row.content, row.id);
+          }
+        );
+      } catch (e) {
+        // something went wrong
+      }
+    }
+  });
+
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
